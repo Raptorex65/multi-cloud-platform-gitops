a56118ac8f62a9e6d3aa988fb450e512635d25f4
Raptorex65 <raptorex109@gmail.com>
2026-05-12 14:34:28 +0200

    Update aws ecr main configuration

--- infra/modules/aws-ecr/main.tf
+++ infra/modules/aws-ecr/main.tf
@@ -10,3 +10,42 @@ resource "aws_ecr_repository" "mc" {
     Name = each.value
   })  
 }
+
+resource "aws_ecr_lifecycle_policy" "api" {
+  for_each = aws_ecr_repository.mc
+  repository = each.value.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Expire untagged images older than 7 days"
+
+        selection = {
+          tagStatus   = "untagged"
+          countType   = "sinceImagePushed"
+          countUnit   = "days"
+          countNumber = 7
+        }
+
+        action = {
+          type = "expire"
+        }
+      },
+      {
+        rulePriority = 2
+        description  = "Keep last 10 images"
+
+        selection = {
+          tagStatus   = "any"
+          countType   = "imageCountMoreThan"
+          countNumber = 5
+        }
+
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
