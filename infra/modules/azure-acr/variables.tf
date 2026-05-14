117923b83a75015ee3e85c650ef5e4f1bcdf3898
Raptorex65 <raptorex109@gmail.com>
2026-05-14 22:09:00 +0200

    Update azure acr variables

---
+++ infra/modules/azure-acr/variables.tf
@@ -0,0 +1,15 @@
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "prefix" {
+  type = string
+}
+
+variable "common_tags" {
+  type = map(string)
+}
