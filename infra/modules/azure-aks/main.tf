dab2d55d0f416c59ca87d33557ceb4d55331acc0
Raptorex65 <raptorex109@gmail.com>
2026-05-14 22:06:30 +0200

    Update azure aks main module

---
+++ infra/modules/azure-aks/main.tf
@@ -0,0 +1,25 @@
+resource "azurerm_kubernetes_cluster" "main" {
+  name                = "${var.prefix}-aks"
+  location            = var.location
+  resource_group_name = var.rg_name
+  dns_prefix          = "${var.prefix}-aks"
+
+  default_node_pool {
+    name           = "system"
+    node_count     = 1
+    vm_size        = "Standard_B2s"
+    vnet_subnet_id = var.aks_subnet_id
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin    = "azure"
+    load_balancer_sku = "standard"
+    outbound_type     = "loadBalancer"
+  }
+
+  tags = var.tags
+}
