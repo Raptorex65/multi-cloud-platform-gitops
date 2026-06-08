d624625a3f8df78086488f6d5669efcf4b15041a
Raptorex65 <raptorex109@gmail.com>
2026-06-08 22:13:10 +0200

    Update azure aks outputs

--- infra/modules/azure-aks/outputs.tf
+++ infra/modules/azure-aks/outputs.tf
@@ -14,3 +14,7 @@ output "kube_config" {
 output "kubelet_identity_object_id" {
   value = azurerm_kubernetes_cluster.main.kubelet_identity[0].object_id
 }
+
+output "oidc_issuer_url" {
+  value = azurerm_kubernetes_cluster.main.oidc_issuer_url
+}
